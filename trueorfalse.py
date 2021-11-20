ae11463f21c4061957f879fbd619214399af376d
oscarthecode <94759104+oscarthecode@users.noreply.github.com>
2021-11-20 13:25:32 -0500

    Create trueorfalse.py

---
+++ trueorfalse.py
@@ -0,0 +1,28 @@
+# python quiz game
+print("Hello, this is a true or false game!")
+print("The rules are simple: a statement is said and you must determine if it is true or false(type t for true and f for false)")
+
+question1 = input("True or False: world war 2 ended in 1949: ")
+
+if question1 == "t":
+    print("Incorrect")
+elif question1 == "f":
+    print("Correct!")
+
+question2 = input("True or False: the c programming language was invented in 1972: ")
+
+if question2 == "t":
+    print("Correct!")
+elif question2 == "f":
+    print("Incorrect")
+
+question3 = input("True or False: Isaac Newton invented fractions: ")
+
+if question1 == "t":
+    print("Incorrect")
+elif question1 == "f":
+    print("Correct!")
+
+print("Thanks for playing! bye!")
+
+input()
